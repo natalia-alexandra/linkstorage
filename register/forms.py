0eb57b107afff1ae1c5267b032e5d1523430d657
Natalia <natalia_alexandra@web.de>
2021-05-28 09:33:28 +0200

    move form from storage into register

--- register/forms.py
+++ register/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core import validators
+
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,3 +11,25 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class FormBasic(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    # valid email
+    verify_email = forms.EmailField(label='enter your email again')
+    text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.ChoiceField(required=False,
+                                   widget=forms.HiddenInput,
+                                   validators=[
+                                       validators.MaxLengthValidator(0)]
+                                   )
+
+    # custom valid form
+    def clean(self):
+        all_clean_date = super().clean()
+        email = all_clean_date['email']
+        vmail = all_clean_date['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("MAKE SURE EMAILs MATCH")
