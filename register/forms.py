6431d1b4fa87164deb2459bd7b79427fba99b637
Natalia <natalia_alexandra@web.de>
2021-06-01 10:28:54 +0200

    improve register

--- register/forms.py
+++ register/forms.py
@@ -1,8 +1,8 @@
+from django.core import validators
 from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.core import validators
 
 
 class RegisterForm(UserCreationForm):
